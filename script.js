4edc3b07f73f5a4fc235cde2d7891365b20b9cdb
Jayesh <jayeshsapkale792@gmail.com>
2024-07-24 20:16:04 +0530

    "remove commented lines"

--- script.js
+++ script.js
@@ -6,21 +6,17 @@ $(document).ready(function () {
 
         if (count == 0) {
 
-            // $("#signUpButton").click(() => {
             $("#signInContainer").hide(1000)
             $("#signUpContainer").show()
             $("#rightContainerText p").text("Already registered?")
             $("#rightContainer button").text("SIGN IN")
-            // })
             count++;
         }
         else {
-            // $("#signUpButton").click(() => {
             $("#signUpContainer").hide(1000)
             $("#signInContainer").show()
             $("#rightContainerText p").text("Register with your personal details to use all of site features")
             $("#rightContainer button").text("SIGN UP")
-            // })
             count = 0;
         }
 
