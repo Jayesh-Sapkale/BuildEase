34d5ceaf46ae9348f80f33e726701428a1dc3b9b
Jayesh <jayeshsapkale792@gmail.com>
2024-07-24 21:43:43 +0530

    "tried some animation"

--- script.js
+++ script.js
@@ -6,21 +6,21 @@ $(document).ready(function () {
 
         if (count == 0) {
 
-            // $("#signUpButton").click(() => {
-            $("#signInContainer").hide(1000)
-            $("#signUpContainer").show()
+            $("#signInContainer").fadeOut(500)
+            setTimeout(() => {
+                $("#signUpContainer").slideToggle(1000)
+            }, 1000);
             $("#rightContainerText p").text("Already registered?")
             $("#rightContainer button").text("SIGN IN")
-            // })
             count++;
         }
         else {
-            // $("#signUpButton").click(() => {
-            $("#signUpContainer").hide(1000)
-            $("#signInContainer").show()
+            $("#signUpContainer").fadeOut(500)
+            setTimeout(() => {
+                $("#signInContainer").slideToggle(1000)
+            }, 1000);
             $("#rightContainerText p").text("Register with your personal details to use all of site features")
             $("#rightContainer button").text("SIGN UP")
-            // })
             count = 0;
         }
 
